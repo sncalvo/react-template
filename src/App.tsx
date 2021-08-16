dbd26e1dbce80be7701a30dabf0ce9d5a60687f8
Santiago Calvo <sncalvo@hotmail.com>
2021-08-15 22:41:47 -0300

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import './App.css';
 
-import Welcome from '@components/Welcome';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+
+import Welcome from '@components/templates/Welcome';
+import About from '@components/templates/About';
 
 const App: React.FC = () => (
-  <div className="App">
-    <Welcome />
-  </div>
+  <BrowserRouter>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/">
+        <Welcome />
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/components/templates/About/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About: React.FC = () => (
+  <div>
+    <h1>Test</h1>
+    <h4>Test 2</h4>
+  </div>
+);
+
+export default About;
